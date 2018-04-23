20a19c8fded797841b8f96230a96f5e217f0a68d
Andy Edwards <jedwards@fastmail.com>
2018-04-23 11:56:53 -0500

    feat: initial commit

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# work-on-repo
+# work-on-pkg
 
-[![Build Status](https://travis-ci.org/jedwards1211/work-on-repo.svg?branch=master)](https://travis-ci.org/jedwards1211/work-on-repo)
-[![Coverage Status](https://codecov.io/gh/jedwards1211/work-on-repo/branch/master/graph/badge.svg)](https://codecov.io/gh/jedwards1211/work-on-repo)
+[![Build Status](https://travis-ci.org/jedwards1211/work-on-pkg.svg?branch=master)](https://travis-ci.org/jedwards1211/work-on-pkg)
+[![Coverage Status](https://codecov.io/gh/jedwards1211/work-on-pkg/branch/master/graph/badge.svg)](https://codecov.io/gh/jedwards1211/work-on-pkg)
 [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
 [![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
 
@@ -10,6 +10,6 @@ my personal script to begin working on a given package, forking it if necessary
 ## Usage
 
 ```sh
-npm install --save work-on-repo
+npm install --save work-on-pkg
 ```
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,81 @@
 /* @flow */
 
-/* eslint-disable no-console, no-undef */
-console.log('Hello world!')
+import {stat} from 'fs-extra'
+import {exec} from 'promisify-child-process'
+import os from 'os'
+import path from 'path'
+
+type Options = {
+  pkg: string,
+  username: string,
+  password: string,
+  organizations?: Array<string>,
+}
+
+function stripScope(pkg: string): string {
+  return pkg.substring(pkg.indexOf('/') + 1)
+}
+
+async function directoryExists(dir: string): Promise<boolean> {
+  try {
+    return (await stat(dir)).isDirectory()
+  } catch (error) {
+    return false
+  }
+}
+
+function parseRepoUrl(url: string): {owner: string, repo: string} {
+  const result = /https:\/\/github.com\/([^\/.]+)\/([^\/.]+)/.exec(url)
+  if (!result) throw new Error(`invalid Github URL: ${url}`)
+  return {owner: result[1], repo: result[2]}
+}
+
+export default async function workOnRepo(options: Options): Promise<any> {
+  const {pkg, username, password, organizations} = options
+  const pkgWithoutScope = stripScope(pkg)
+  const workDir = path.join(os.homedir(), pkgWithoutScope)
+  if (await directoryExists(workDir)) {
+    /* eslint-disable no-console */
+    console.log(`cd ${workDir}`)
+    console.log(`git checkout master`)
+    console.log(`git pull origin master`)
+    console.log(`git pull upstream master`)
+    console.log(`subl .`)
+    /* eslint-enable no-console */
+  } else {
+    const octokit = require('@octokit/rest')()
+    await octokit.authenticate({
+      type: 'basic',
+      username,
+      password,
+    })
+
+    const repoUrl = (await exec(`npm view ${pkg} repository.url`)).stdout.trim()
+    const {owner, repo} = parseRepoUrl(repoUrl)
+    let forkOwner = username
+    if (organizations) {
+      for (let organization of organizations) {
+        try {
+          await octokit.activity.getEventsForRepo({owner: organization, repo})
+          forkOwner = organization
+          break
+        } catch (error) {
+          // ignore
+        }
+      }
+    }
+    if (forkOwner === username) {
+      await octokit.repos.fork({owner, repo})
+    } else {
+      await octokit.repos.fork({owner, repo, organization: forkOwner})
+    }
+    /* eslint-disable no-console */
+    console.log(`cd ${os.homedir()}`)
+    console.log(`git clone https://github.com/${forkOwner}/${repo}.git`)
+    console.log(`cd ${repo}`)
+    console.log(`git remote add upstream https://github.com/${owner}/${repo}.git`)
+    console.log(`yarn --ignore-scripts`)
+    console.log(`subl .`)
+    /* eslint-enable no-console */
+  }
+}
